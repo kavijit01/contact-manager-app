f1fb52fddb4ff8879883f115917bc48a04ad3ce1
avijitkundu <k.avijit01@gmail.com>
2021-06-09 11:01:44 +0530

    header footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateContactComponent } from './create-contact/create-contact.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateContactComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
